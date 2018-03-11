7b85da99ab73ab2cef7f4341d329000d479b7494
ninjadev999 <mikenarkss@gmail.com>
2018-03-11 10:54:12 -0700

    updated

--- lib/transportapp_error.rb
+++
@@ -1,2 +0,0 @@
-class TransportappError < RuntimeError
-end
--- lib/utility_methods.rb
+++
@@ -1,9 +0,0 @@
-# makes a basic string HTML/Javascript-safe, escaping ' and "
-def hs(text)
-  text.gsub!("'","&#39;")
-end
-
-# offers time in convenient Excel format
-def time_string(the_time)
-  return the_time.strftime("%Y_%m_%d %H:%M")
-end
