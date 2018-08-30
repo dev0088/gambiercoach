3914d821790a529c38b1066490524b7bd05aec50
ninjadev999 <mikenarkss@gmail.com>
2018-08-30 07:00:58 -0700

    fixed emailing issue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,25 +34,6 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.default_options = {from: 'vigd@kenyon.edu'}
-  # config.action_mailer.smtp_settings = {
-  #   address:              'smtp.gmail.com',
-  #   port:                 587,
-  #   domain:               'gambiercoach.com',
-  #   user_name:            'valeriiadidushok',
-  #   password:             'valeriia123qwe!@#QWE',
-  #   authentication:       'plain',
-  #   enable_starttls_auto: true }
-
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.default_options = {from: 'valeriiadidushok@gmail.com'}
-  # config.action_mailer.smtp_settings = {
-  #   address:              'smtp.gmail.com',
-  #   port:                 587,
-  #   domain:               'gambiercoach.com',
-  #   user_name:            'valeriiadidushok',
-  #   password:             'valeriia123qwe!@#QWE',
-  #   authentication:       'plain',
-  #   enable_starttls_auto: true }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
